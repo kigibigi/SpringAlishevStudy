365a3b0c5c0fa54d3894c02bd2c2d8459d258af1
Boris <b.molchanov.90@gmail.com>
2025-02-16 15:44:42 +0300

    lesson 6

--- src/main/java/com/bmolc/music/MusicPlayer.java
+++ src/main/java/com/bmolc/music/MusicPlayer.java
@@ -5,12 +5,43 @@ import com.bmolc.music.Music;
 public class MusicPlayer {
     private Music music;
 
+    private String name;
+    private int volume;
+
     //IoC(здесь происходит внедрение зависимости)
     public MusicPlayer(Music music){
         this.music = music;
     }
 
+    public MusicPlayer() {
+    }
+
     public void playMusic() {
         System.out.println("Playing: " + music.getSong());
     }
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
+    public Music getMusic() {
+        return music;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
 }
